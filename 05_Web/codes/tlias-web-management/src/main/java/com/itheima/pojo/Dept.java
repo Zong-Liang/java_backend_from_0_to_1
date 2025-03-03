c556818e91d8528836e18d312cbd6054ca2ab2cc
Zong-Liang <zongliang0726@163.com>
2025-03-03 14:47:43 +0800

    update

---
+++ 05_Web/codes/tlias-web-management/src/main/java/com/itheima/pojo/Dept.java
@@ -0,0 +1,16 @@
+package com.itheima.pojo;
+
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dept {
+    private Integer id;
+    private String name;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
