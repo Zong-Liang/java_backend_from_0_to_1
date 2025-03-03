6d67de16228e948a31a8870555b63e2c5cf2f864
Zong-Liang <zongliang0726@163.com>
2025-03-03 18:19:01 +0800

    update

--- 05_Web/codes/tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
+++ 05_Web/codes/tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -1,6 +1,8 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.Dept;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,4 +13,17 @@ public interface DeptMapper {
 
     @Select("select id, name, create_time, update_time from dept order by update_time desc")
     List<Dept> findAll();
+
+    @Delete("delete from dept where id = #{id}")
+    void deleteById(Integer id);
+
+    @Insert("insert into dept(name, create_time, update_time) values(#{name}, #{createTime}, #{updateTime})")
+//    @Insert("insert into dept(name, create_time, update_time) values(#{name}, now(), now())")
+    void add(Dept dept);
+
+    @Select("select id, name, create_time, update_time from dept where id = #{id}")
+    Dept getById(Integer id);
+
+    @Select("update dept set name = #{name}, update_time = #{updateTime} where id = #{id}")
+    void update(Dept dept);
 }
--- 05_Web/codes/tlias-web-management/src/main/java/com/itheima/service/impl/DeptServiceImpl.java
+++ 05_Web/codes/tlias-web-management/src/main/java/com/itheima/service/impl/DeptServiceImpl.java
@@ -6,6 +6,7 @@ import com.itheima.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,5 +17,30 @@ public class DeptServiceImpl implements DeptService {
     @Override
     public List<Dept> findAll() {
         return deptMapper.findAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        deptMapper.deleteById(id);
+    }
+
+    @Override
+    public void add(Dept dept) {
+        //补全基础属性
+        dept.setCreateTime(LocalDateTime.now());
+        dept.setUpdateTime(LocalDateTime.now());
+        deptMapper.add(dept);
+    }
+
+    @Override
+    public Dept getById(Integer id) {
+        return deptMapper.getById(id);
+    }
+
+    @Override
+    public void update(Dept dept) {
+        //补全基础属性
+        dept.setUpdateTime(LocalDateTime.now());
+        deptMapper.update(dept);
     }
 }
