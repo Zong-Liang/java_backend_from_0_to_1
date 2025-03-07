f8a3df42a7678bf5db278db082a3b035ac6e73d9
ZongLiang <zongliang0726@163.com>
2025-03-07 12:34:01 +0800

    update

--- 06_projects/test_mybatisplus/mybatisplus/src/test/java/com/mybatisplus/MybatisplusApplicationTest.java
+++ 06_projects/test_mybatisplus/mybatisplus/src/test/java/com/mybatisplus/MybatisplusApplicationTest.java
@@ -1,6 +1,8 @@
 package com.mybatisplus;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mybatisplus.mapper.DeptMapper;
+import com.mybatisplus.pojo.Dept;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,8 +13,16 @@ class MybatisplusApplicationTest {
     @Autowired
     DeptMapper deptMapper;
 
+
     @Test
-    void contextLoads() {
-        System.out.println(deptMapper.selectById(1));
+    void test1() {
+        deptMapper.selectById(1);
     }
-}
+    
+    @Test
+    void test2() {
+        System.out.println("分页查询");
+        Page<Dept> page = new Page<>(1, 2); // 创建分页对象
+        deptMapper.selectPage(page, null);// 执行分页查询
+    }
+    }
